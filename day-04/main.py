e418b831faffbfc59286c07d6c1e40291bf5494e
David Zulaica <david@zulaica.info>
2022-12-06 00:13:29 -0800

    Day 4, Part 2

--- day-04/main.py
+++ day-04/main.py
@@ -6,7 +6,7 @@ from utils.input import parse_input
 
 
 def input():
-    return [line.strip().split(",") for line in parse_input("input.txt")]
+    return [line.strip().split(",") for line in parse_input()]
 
 
 def unpack_section_ranges():
@@ -19,19 +19,20 @@ def unpack_section_ranges():
     ]
 
 
-def get_fully_contained_assignments():
+def get_assignment_pair_results(fn=all):
     return [
-        int(
-            all(section in assignment_a for section in assignment_b)
-            or all(section in assignment_b for section in assignment_a)
-        )
+        fn(section in assignment_a for section in assignment_b)
+        or fn(section in assignment_b for section in assignment_a)
         for assignment_a, assignment_b in unpack_section_ranges()
     ]
 
 
 def main():
     print(
-        f"Number of assignment pairs where one range is fully contained: {sum(get_fully_contained_assignments())}"
+        f"Number of assignment pairs where one range is fully contained: {sum(get_assignment_pair_results(all))}"
+    )
+    print(
+        f"Number of assignment pairs with overlapping ranges: {sum( get_assignment_pair_results(any))}"
     )
 
 
