1b1d65fa87fbe6ce5fdc5d7ca7c7726a35abf4f9
David Zulaica <david@zulaica.info>
2022-12-05 18:48:10 -0800

    Day 3, Part 2

--- day-03/main.py
+++ day-03/main.py
@@ -4,28 +4,43 @@ sys.path.append("../")
 
 from utils.input import parse_input
 
-UPPER_OFFSET = 38
 LOWER_OFFSET = 96
+UPPER_OFFSET = 38
 
 
-def format_input():
+def rucksack_input():
     return [
         [line[: len(line) // 2], line[len(line) // 2 :]]
         for line in parse_input("input.txt")
     ]
 
 
-def get_duplicates():
-    return [list(set(a).intersection(b))[0] for a, b in format_input()]
+def group_input():
+    input = parse_input("input.txt")
+    return [input[line : line + 3] for line in range(0, len(input), 3)]
+
+
+def get_duplicates(input):
+    return [
+        list(set.intersection(*[set(item.strip()) for item in items]))[0]
+        for items in input
+    ]
 
 
 def get_item_priority(item):
     return ord(item) - UPPER_OFFSET if item.isupper() else ord(item) - LOWER_OFFSET
 
 
+def get_priority_sum(input):
+    return sum([get_item_priority(duplicate) for duplicate in get_duplicates(input)])
+
+
 def main():
     print(
-        f"The sum of the priority of duplicates is: {sum([get_item_priority(duplicate) for duplicate in get_duplicates()])}"
+        f"The sum of the priority of rucksack duplicates is: {get_priority_sum(rucksack_input())}"
+    )
+    print(
+        f"The sum of the priority of group duplicates is: {get_priority_sum(group_input())}"
     )
 
 
