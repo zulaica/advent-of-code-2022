71b62cce2532573541f07011d2a99cfd28ab0102
David Zulaica <david@zulaica.info>
2022-12-12 20:35:52 -0800

    Day 7, Part 2

--- day-07/main.py
+++ day-07/main.py
@@ -7,30 +7,56 @@ sys.path.append("../")
 from utils.input import parse_input
 
 
+ROOT = PurePath("ROOT")
+
+
+def get_directory_paths(directories):
+    return [
+        PurePath("/".join(item))
+        for item in sorted(
+            [path.parts for path in directories.keys()], key=len, reverse=True
+        )
+    ]
+
+
 def get_directory_sizes():
-    directory_sizes = defaultdict(int)
-    path = PurePath("ROOT")
+    directories = defaultdict(int)
+    files = {}
+    path = ROOT
 
     for line in parse_input():
         if line in ["$ ls", "$ cd /"] or line.startswith("dir "):
             continue
 
         if line.endswith(".."):
-            directory_sizes[path.parent] += directory_sizes[path]
             path = path.parent
         elif line.startswith("$ cd"):
             path = PurePath.joinpath(path, line.split()[-1])
+            directories[path]
         else:
-            directory_sizes[path] += int(line.split()[0])
+            files[PurePath.joinpath(path, line.split()[-1])] = int(line.split()[0])
 
-    for parent in path.parents[:-1]:
-        directory_sizes[parent] += directory_sizes[path]
+    for filepath, size in files.items():
+        directories[filepath.parent] += size
 
-    return directory_sizes
+    for path in get_directory_paths(directories):
+        directories[path.parent] += directories[path]
+
+    return directories
 
 
 if __name__ == "__main__":
+    directory_sizes = get_directory_sizes()
+    TOTAL_SPACE = 70_000_000
+    REQUIRED_SPACE = 30_000_000
+    USED_SPACE = directory_sizes[ROOT]
+    FREE_SPACE = TOTAL_SPACE - USED_SPACE
+    NEEDED_SPACE = REQUIRED_SPACE - FREE_SPACE
     print(
         "The sum of all directores with sizes less than 100,000:",
-        f"{sum([size for size in get_directory_sizes().values() if size <= 100_000])}",
+        f"{sum(size for size in directory_sizes.values() if size <= 100_000)}",
+    )
+    print(
+        "The size of the smallest directory that can be deleted to free enough space:",
+        f"{min(size for size in directory_sizes.values() if size >= NEEDED_SPACE)}",
     )
